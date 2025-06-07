28b24e71ac90366141d132584ba04fc3057fc857
KymaiselHunter <kyle.bautista78@myhunter.cuny.edu>
2025-06-06 21:42:52 -0400

    aws lambda support

--- lib/website-infrastructure-cdk-stack.ts
+++ lib/website-infrastructure-cdk-stack.ts
@@ -3,21 +3,24 @@ import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+
 export class WebsiteInfrastructureCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'WebsiteInfrastructureCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-
     new s3.Bucket(this, 'ImageGalleryBucket', {
       versioned: true,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true
     })
+
+    const pingFunction = new lambda.Function(this, 'PingFunction',{
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'index.handler', 
+      code: lambda.Code.fromAsset('lambdas/ping'),
+      functionName: 'PingTestFunction', // Optional: added for clarity
+    });
   }
 }
